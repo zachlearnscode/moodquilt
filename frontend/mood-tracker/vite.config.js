76b7ad26d0735fd3b4e53fb6bc3c488f6ea524d9
Zachary McNulty <zach@Zachs-MacBook.attlocal.net>
2025-04-05 11:19:51 -0500

    Adding tailwindcss

--- frontend/mood-tracker/vite.config.js
+++ frontend/mood-tracker/vite.config.js
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import tailwindcss from '@tailwindcss/vite'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -11,6 +12,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    tailwindcss()
   ],
   resolve: {
     alias: {
